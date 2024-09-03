d0a10bafb5a0e3367f1bf1a11121679d712a3420
SGESissyphus <simon.povh@lessing-ffm.net>
2024-09-03 20:44:48 +0200

    first version Rubric added

---
+++ src/graderPrivate/java/h05/H05_RubricProvider.java
@@ -0,0 +1,121 @@
+package h06;
+
+import org.sourcegrade.jagr.api.rubric.*;
+import static org.tudalgo.algoutils.tutor.general.jagr.RubricUtils.criterion;
+
+public class H05_RubricProvider implements RubricProvider {
+
+    private static final Criterion H5_1_1 = Criterion.builder()
+        .shortDescription("H5.1.1 | Flying Interface")
+        .maxPoints(1)
+        .criterion("The Flying interface is implemented correctly.")
+        .build();
+
+    private static final Criterion H5_1_2 = Criterion.builder()
+        .shortDescription("H5.1.2 | FuelType")
+        .maxPoints(2)
+        .addChildCriteria(
+            criterion("The FuelType enum is implemented correctly and with the correct values.")
+            criterion("The Constructor of the FuelType enum is implemented correctly.")
+        )
+        .build();
+
+    private static final Criterion H5_1_3 = Criterion.builder()
+        .shortDescription("H5.1.3 | Plane")
+        .maxPoints(7)
+        .addChildCriteria(
+            criterion("The Interface is correctly implemented into the Plane class.")
+            criterion("The Plane class has the correct attributes.")
+            criterion("The mass method functions correctly.")
+            criterion("Refuel prints the right message.")
+            criterion("The getFuelConsumptionPerKilometer method is implemented correctly.")
+            criterion("The fly method is implemented correctly.")
+            criterion("The takeOff method is implemented correctly.")
+        )
+        .build();
+
+    private static final Criterion H5_1_4 = Criterion.builder()
+        .shortDescription("H5.1.4 | Weather Balloon")
+        .maxPoints(2)
+        .addChildCriteria(
+            criterion("The methods start and pop are implemented correctly.")
+            criterion("THe WeatherBalloon class is implemented correctly.")
+        )
+        .build();
+
+    private static final Criterion H5_2_1 = Criterion.builder()
+        .shortDescription("H5.2.1 | CarriesPassengers Interface")
+        .maxPoints(4)
+        .addChildCriteria(
+            criterion("The CarriesPassengers interface is implemented correctly.")
+            criterion("The PassengerPlane class correctly implements the CarriesPassengers interface.")
+            criterion("The board and disembark methods and getPassengerCount are implemented correctly.")
+            criterion("The constants and constructor are implemented correctly.")
+        )
+        .build();
+
+    private static final Criterion H5_2_2 = Criterion.builder()
+        .shortDescription("H5.2.2 | CarriesCargo Interface")
+        .maxPoints(4)
+        .addChildCriteria(
+            criterion("The CarriesCargo interface is implemented correctly.")
+            criterion("The CargoPlane class correctly implements the CarriesCargo interface.")
+            criterion("The loadContainer, hasFreightLoaded and unloadNextContainer methods are implemented correctly.")
+            criterion("The mass method and constructor are implemented correctly.")
+        )
+        .build();
+
+    private static final Criterion H5_2_3 = Criterion.builder()
+        .shortDescription("H5.2.3 | CombinedPlane")
+        .maxPoints(2)
+        .addChildCriteria(
+            criterion("The CombinedPlane class correctly implements the CarriesPassengers and CarriesCargo interfaces.")
+            criterion("The class is completly correct.")
+        )
+        .build();
+
+    private static final Criterion H5_3_1 = Criterion.builder()
+        .shortDescription("H5.3.1 | Refuelling")
+        .maxPoints(1)
+        .criterion("The Refuelling Interface is implemented correctly.")
+        .build();
+
+    private static final Criterion H5_3_2 = Criterion.builder()
+        .shortDescription("H5.3.2 | Tank")
+        .maxPoints(2)
+        .addChildCriteria(
+            criterion("The Tank class is implemented correctly and with the right attributes.")
+            criterion("The refuelPlane method is implemented correctly.")
+        )
+        .build();
+
+    private static final Criterion H5_3_3 = Criterion.builder()
+        .shortDescription("H5.3.3 | TankerPlane")
+        .maxPoints(3)
+        .addChildCriteria(
+            criterion("The TankerPlane class implements the right interfaces and the constructor is implemented correctly.")
+            criterion("The loadFuel and mass methods are implemented correctly.")
+            criterion("The refuelPlane method is implemented correctly.")
+        )
+        .build();
+
+    private static final Criterion H5_4 = Criterion.builder()
+        .shortDescription("H5.4 | Runway")
+        .maxPoints(2)
+        .addChildCriteria(
+            criterion("The Runway class is implemented correctly and with the right attributes.")
+            criterion("The methods land method is implemented correctly.")
+        )
+        .build();
+
+    private static final Criterion H5_5 = Criterion.builder()
+        .shortDescription("H5.5 | Airspace")
+        .maxPoints(0)
+        .build();
+
+    private static final Criterion H5_6 = Criterion.builder()
+        .shortDescription("H5.6 | Spielwiese")
+        .maxPoints(0)
+        .build();
+
+}
--- src/main/java/h05/Main.java
+++ src/main/java/h05/Main.java
@@ -13,7 +13,6 @@ public class Main {
 
         /*
         Die konkreten Zahlen sind noch nicht final und können sich ändern.
-        TODO: Wie kann instanceof genutzt werden?
          */
         Runway runway01 = new Runway(2000);
         Runway runway02 = new Runway(4000);
@@ -23,18 +22,18 @@ public class Main {
 
         Tank jetATank = new Tank(FuelType.JetA);
         Tank jetBTank = new Tank(FuelType.JetB);
-        TankerPlane tankerPlane = new TankerPlane("D-ABCD", 10000, FuelType.JetA, 1000000);
+        TankerPlane tankerPlane = new TankerPlane("D-ABCD", 10000, FuelType.JetA, 1000);
         tankerPlane.loadFuel(FuelType.AvGas, 100000);
         tankerPlane.loadFuel(FuelType.Biokerosin, 100000);
 
-        PassengerPlane passengerPlane = new PassengerPlane("GAG-67", 10000, FuelType.JetA, 1070000, 5);
+        PassengerPlane passengerPlane = new PassengerPlane("GAG-67", 10000, FuelType.JetA, 1700, 5);
         jetATank.refuel(passengerPlane);
         passengerPlane.board(100);
         passengerPlane.takeOff();
 
         Airspace.get().scanAirspace();
 
-        CargoPlane cargoPlane = new CargoPlane("D-AFFF", 8000, FuelType.JetB, 15000000);
+        CargoPlane cargoPlane = new CargoPlane("D-AFFF", 8000, FuelType.JetB, 1500);
         cargoPlane.loadContainer(1000);
         jetBTank.refuel(cargoPlane);
 
@@ -43,7 +42,7 @@ public class Main {
         cargoPlane.takeOff();
         cargoPlane.fly(1000);
 
-        CombinedPlane combinedPlane = new CombinedPlane("D-ABCD", 9000, FuelType.AvGas, 1070000, 5);
+        CombinedPlane combinedPlane = new CombinedPlane("D-ABBB", 9000, FuelType.AvGas, 10700, 5);
         tankerPlane.refuel(combinedPlane);
 
         combinedPlane.board(30);
@@ -52,12 +51,7 @@ public class Main {
         combinedPlane.fly(3000);
         Airspace.get().scanAirspace();
 
-
-        if(combinedPlane instanceof PassengerPlane){
-            runway01.land(combinedPlane);
-        }else {
-            runway02.land(combinedPlane);
-        }
+        runway01.land(combinedPlane);
         runway02.land(cargoPlane);
 
         Airspace.get().scanAirspace();
