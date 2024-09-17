c6f0efdb8f94888bfed8ca2ece838c35cc32e14b
Daniel Mangold <mangold.daniel@protonmail.com>
2024-09-17 09:59:51 +0200

    Add test refs to rubric provider

--- src/graderPrivate/java/h05/H05_RubricProvider.java
+++ src/graderPrivate/java/h05/H05_RubricProvider.java
@@ -1,6 +1,8 @@
 package h05;
 
 import org.sourcegrade.jagr.api.rubric.*;
+import org.tudalgo.algoutils.tutor.general.json.JsonParameterSet;
+
 import static org.tudalgo.algoutils.tutor.general.jagr.RubricUtils.criterion;
 
 @SuppressWarnings("unused")
@@ -10,7 +12,9 @@ public class H05_RubricProvider implements RubricProvider {
         .shortDescription("H5.1.1 | Flying Interface")
         .maxPoints(1)
         .addChildCriteria(
-            criterion("Das Flying-Interface ist korrekt implementiert.")
+            criterion("Das Flying-Interface ist korrekt implementiert.",
+                JUnitTestRef.ofMethod(() -> FlyingTest.class.getDeclaredMethod("testHeader")),
+                JUnitTestRef.ofMethod(() -> FlyingTest.class.getDeclaredMethod("testMethods")))
         )
         .build();
 
@@ -18,8 +22,12 @@ public class H05_RubricProvider implements RubricProvider {
         .shortDescription("H5.1.2 | FuelType")
         .maxPoints(2)
         .addChildCriteria(
-            criterion("Das FuelType-Enum ist korrekt implementiert und enthält die richtigen Werte.")
-            criterion("Der Konstruktor des FuelType-Enums ist korrekt implementiert.")
+            criterion("Das FuelType-Enum ist korrekt implementiert und enthält die richtigen Werte.",
+                JUnitTestRef.ofMethod(() -> FuelTypeTest.class.getDeclaredMethod("testHeader")),
+                JUnitTestRef.ofMethod(() -> FuelTypeTest.class.getDeclaredMethod("testFields")),
+                JUnitTestRef.ofMethod(() -> FuelTypeTest.class.getDeclaredMethod("testEnumConstants", JsonParameterSet.class))),
+            criterion("Der Konstruktor des FuelType-Enums ist korrekt implementiert.",
+                JUnitTestRef.ofMethod(() -> FuelTypeTest.class.getDeclaredMethod("testConstructor")))
         )
         .build();
 
@@ -27,11 +35,11 @@ public class H05_RubricProvider implements RubricProvider {
         .shortDescription("H5.1.3 | Flugzeug")
         .maxPoints(6)
         .addChildCriteria(
-            criterion("Das Interface ist korrekt in die Flugzeugklasse implementiert.")
-            criterion("Die Flugzeugklasse hat die richtigen Attribute.")
-            criterion("Die Masse-Methode funktioniert korrekt.")
-            criterion("Die Refuel-Methode gibt die richtige Nachricht aus.")
-            criterion("Die getFuelConsumptionPerKilometer-Methode ist korrekt implementiert.")
+            criterion("Das Interface ist korrekt in die Flugzeugklasse implementiert."),
+            criterion("Die Flugzeugklasse hat die richtigen Attribute."),
+            criterion("Die Masse-Methode funktioniert korrekt."),
+            criterion("Die Refuel-Methode gibt die richtige Nachricht aus."),
+            criterion("Die getFuelConsumptionPerKilometer-Methode ist korrekt implementiert."),
             criterion("Die Methoden fly und takeoff sind korrekt implementiert.")
         )
         .build();
@@ -53,9 +61,9 @@ public class H05_RubricProvider implements RubricProvider {
         .shortDescription("H5.2.1 | CarriesPassengers Interface")
         .maxPoints(4)
         .addChildCriteria(
-            criterion("Das CarriesPassengers-Interface ist korrekt implementiert.")
-            criterion("Die PassengerPlane-Klasse implementiert das CarriesPassengers-Interface korrekt.")
-            criterion("Die Methoden board, disembark und getPassengerCount sind korrekt implementiert.")
+            criterion("Das CarriesPassengers-Interface ist korrekt implementiert."),
+            criterion("Die PassengerPlane-Klasse implementiert das CarriesPassengers-Interface korrekt."),
+            criterion("Die Methoden board, disembark und getPassengerCount sind korrekt implementiert."),
             criterion("Die Konstanten und der Konstruktor sind korrekt implementiert.")
         )
         .build();
@@ -64,9 +72,9 @@ public class H05_RubricProvider implements RubricProvider {
         .shortDescription("H5.2.2 | CarriesCargo Interface")
         .maxPoints(4)
         .addChildCriteria(
-            criterion("Das CarriesCargo-Interface ist korrekt implementiert.")
-            criterion("Die CargoPlane-Klasse implementiert das CarriesCargo-Interface korrekt.")
-            criterion("Die Masse-Methode und der Konstruktor sind korrekt implementiert.")
+            criterion("Das CarriesCargo-Interface ist korrekt implementiert."),
+            criterion("Die CargoPlane-Klasse implementiert das CarriesCargo-Interface korrekt."),
+            criterion("Die Masse-Methode und der Konstruktor sind korrekt implementiert."),
             criterion("Die Methoden loadContainer, hasFreightLoaded und unloadNextContainer sind korrekt implementiert.")
         )
         .build();
@@ -75,7 +83,7 @@ public class H05_RubricProvider implements RubricProvider {
         .shortDescription("H5.2.3 | Kombiniertes Flugzeug")
         .maxPoints(2)
         .addChildCriteria(
-            criterion("Die CombinedPlane-Klasse implementiert die Interfaces CarriesPassengers und CarriesCargo korrekt.")
+            criterion("Die CombinedPlane-Klasse implementiert die Interfaces CarriesPassengers und CarriesCargo korrekt."),
             criterion("Die Klasse ist vollständig korrekt.")
         )
         .build();
@@ -97,7 +105,7 @@ public class H05_RubricProvider implements RubricProvider {
         .shortDescription("H5.3.2 | Tank")
         .maxPoints(2)
         .addChildCriteria(
-            criterion("Die Tank-Klasse ist korrekt implementiert und enthält die richtigen Attribute.")
+            criterion("Die Tank-Klasse ist korrekt implementiert und enthält die richtigen Attribute."),
             criterion("Die Methode refuelPlane ist korrekt implementiert.")
         )
         .build();
@@ -106,7 +114,7 @@ public class H05_RubricProvider implements RubricProvider {
         .shortDescription("H5.3.3 | Tanker-Flugzeug")
         .maxPoints(2)
         .addChildCriteria(
-            criterion("Die TankerPlane-Klasse implementiert die richtigen Interfaces und der Konstruktor ist korrekt implementiert.")
+            criterion("Die TankerPlane-Klasse implementiert die richtigen Interfaces und der Konstruktor ist korrekt implementiert."),
             criterion("Die Methoden loadFuel, mass und refuelPlane sind korrekt implementiert.")
         )
         .build();
@@ -120,7 +128,7 @@ public class H05_RubricProvider implements RubricProvider {
         .shortDescription("H5.4 | Start- und Landebahn")
         .maxPoints(2)
         .addChildCriteria(
-            criterion("Die Runway-Klasse ist korrekt implementiert und enthält die richtigen Attribute.")
+            criterion("Die Runway-Klasse ist korrekt implementiert und enthält die richtigen Attribute."),
             criterion("Die Methode land ist korrekt implementiert.")
         )
         .build();
@@ -137,7 +145,7 @@ public class H05_RubricProvider implements RubricProvider {
         .shortDescription("H5.6 | Spielwiese")
         .maxPoints(2)
         .addChildCriteria(
-            criterion("Mindestens 10 Punkte sind korrekt implementiert.")
+            criterion("Mindestens 10 Punkte sind korrekt implementiert."),
             criterion("Alle Punkte sind korrekt implementiert.")
         )
         .build();
